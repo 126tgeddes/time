e286c79c994781050eb3fb5d404b70eaa6317ece
Timothy <126tgeddes@frhsd.com>
2024-11-06 12:33:19 -0500

    11/6

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,30 +1,25 @@
-// Create new Date object
+
 const d = new Date();
-// Store the value of getHours in variable
 let hour = d.getHours();
-// hour = 15;
-// hour = 20;
-// hour = 2;
-// console.log(hour);
-// Create greeting variable
 let greetingText = document.getElementById("greeting-text");
 
 if (hour < 12) {
-  // if hour is less than 12pm
-  greetingText.textContent = "Good Morning!";
-  document.querySelector(".night").classList.add("d-none");
-  document.querySelector(".afternoon").classList.add("d-none");
+    greetingText.textContent = "Good Morning!";
+    document.querySelector(".afternoon").classList.add("d-none");
+    document.querySelector(".night").classList.add("d-none");
+    document.querySelector(".morning").classList.remove("d-none");
 } else if (hour < 17) {
-  // if hour is less than 17:00 (but greater than 12pm)
-  greetingText.textContent = "Good Afternoon!";
-  document.getElementsByTagName("body")[0].classList.add("noon-bg");
-  greetingText.parentElement.classList.add("noon-gradient");
-  document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".night").classList.add("d-none");
-} // hour is greater than 17:00
-else {
-  greetingText.textContent = "Good Night!";
-  greetingText.parentElement.classList.add("night-gradient");
-  document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".afternoon").classList.add("d-none");
+    greetingText.textContent = "Good Afternoon!";
+    document.querySelector("body").classList.add("noon-bg");
+    greetingText.parentElement.classList.add("noon-gradient");
+    document.querySelector(".morning").classList.add("d-none");
+    document.querySelector(".night").classList.add("d-none");
+    document.querySelector(".afternoon").classList.remove("d-none");
+} else {
+    greetingText.textContent = "Good Night!";
+    greetingText.parentElement.classList.add("night-gradient");
+    document.querySelector(".morning").classList.add("d-none");
+    document.querySelector(".afternoon").classList.add("d-none");
+    document.querySelector(".night").classList.remove("d-none");
 }
+
